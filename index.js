dcb70aacba506aac13785fe8694276a867c842bf
Kartikay Bagla <kartikaybagla@gmail.com>
2021-09-21 19:47:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var diff = parseInt(document.querySelector('title').text);
+
+var prevDate = null;
+
+function updateImage() {
+    var currDate = new Date();
+    currDate.setDate(currDate.getDate() - diff);
+
+    if (currdate != prevDate) {
+        prevDate = currDate;
+        var dd = String(currDate.getDate()).padStart(2, '0');
+        var mm = String(currDate.getMonth() + 1).padStart(2, '0');
+        var yyyy = String(currDate.getFullYear());
+
+        currDate = yyyy + "-" + mm + "-" + dd;
+
+        var url = "http://apodapi.herokuapp.com/api?date=" + currDate;
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(jsonObj => {
+                document.body.style.backgroundImage = "url(" + jsonObj["url"] + ")";
+                console.log(jsonObj["url"]);
+            });
+    }
+}
+
+updateImage();
+setTimeout(updateImage, 5000);
